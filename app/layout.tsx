06b88a8d4cfe475c1a54827c11ab10955dfaaf45
naji-97 <elbairabennaji@gmail.com>
2025-11-01 16:08:23 +0100

    setup the tRPC

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import {TRPCReactProvider} from "@/trpc/client"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -20,6 +21,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
+    <TRPCReactProvider>
     <html lang="en">
       <body
         className={`${inter.className}  antialiased`}
@@ -28,5 +30,6 @@ export default function RootLayout({
         {children}
       </body>
     </html>
+    </TRPCReactProvider>
   );
 }
