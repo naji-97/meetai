d8cc88d8930e5d10f63594c4b62c8dfea89486d0
naji-97 <elbairabennaji@gmail.com>
2025-11-03 16:17:10 +0100

    integrate tRcp and and use it int the agents page

--- components/home/ui/HomeView.tsx
+++ components/home/ui/HomeView.tsx
@@ -7,12 +7,10 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 
 export default function Home() {
-    const trpc = useTRPC();
-    // const greeting = useQuery(trpc.hello.queryOptions({ text: 'world' }));
-    const {data}= useQuery(trpc.hello.queryOptions({text:"world"}));
+    
     return (
         <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-            {data?.greeting}
+            Home view
         </div>
     );
 }
