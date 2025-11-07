e0ea89661037df107b1d97c38c192d8945755df5
naji-97 <elbairabennaji@gmail.com>
2025-11-07 13:49:15 +0100

    create an agent data table to display the meetings

--- components/agents/AgentsView.tsx
+++ components/agents/AgentsView.tsx
@@ -4,41 +4,47 @@ import { useQuery, useSuspenseQueries, useSuspenseQuery } from '@tanstack/react-
 import React, { use } from 'react'
 import LoadingState from '../loading-state/LoadingState';
 import ErrorState from '../error-state/ErrorState';
+import { DataTable } from './data-table';
+import { columns } from './columns';
+import EmptyState from '../empty-state/EmptyState';
+
 
 export const AgentsView = () => {
-    const trpc = useTRPC();
-    const {data} =useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
 
-    // if (isLoading) {
-    //     return <LoadingState
-    //     title='Loading agents'
-    //     description='This may take a few seconds...'
-    //     />;
-    // }
-    // if (isError) {
-    //     return <ErrorState
-    //     title='Error loading agents'
-    //     description='Please try again later.'
-    //     />;
-    // }
+  // if (isLoading) {
+  //     return <LoadingState
+  //     title='Loading agents'
+  //     description='This may take a few seconds...'
+  //     />;
+  // }
+  // if (isError) {
+  //     return <ErrorState
+  //     title='Error loading agents'
+  //     description='Please try again later.'
+  //     />;
+  // }
   return (
-    <div>{JSON.stringify(data, null, 2)}</div>
+    <div className='flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4'><DataTable data={data} columns={columns} />
+      {data?.length === 0 && <EmptyState title='Create your first agent' description='Create an agent to join your meetings. Each agent will follow your instructions and can interact with participants during the call.' />}
+    </div>
   )
 }
 
 
 
 export const AgentsViewLoading = () => {
-  return (  
-        <LoadingState title='Loading Agents' description='This may take a few seconds...' />
+  return (
+    <LoadingState title='Loading Agents' description='This may take a few seconds...' />
   )
 }
 
-export const AgentsViewError= () => {
+export const AgentsViewError = () => {
   return (
     <ErrorState
-    title='Error loading Agents'
-    description='Please try again later.'
+      title='Error loading Agents'
+      description='Please try again later.'
     />
   )
 }
--- components/error-state/ErrorState.tsx
+++ components/error-state/ErrorState.tsx
@@ -7,7 +7,7 @@ interface Props {
 }
 const ErrorState = ({title, description}: Props) => {
   return (
-    <div className='py-4 flex flex-1 items-center justify-center'>
+    <div className='py-4 px-8 flex flex-1 items-center justify-center'>
         <div className='flex flex-col items-center justify-center gap-y-6 bg-background rounded-lg p-10 shadow-sm'>
             <AlertCircleIcon className=' size-6 animate-spin text-red-500' />
             <div className='flex flex-col gap-y-2 text-center'>
